8dccb7df84f71ad6eaafdca192e5b5637e6e42b8
Yury Shapkarin <shapkarin@narr8.me>
2014-06-16 01:12:08 +0400

    phantom.js

---
+++ test.js
@@ -0,0 +1,17 @@
+var page = require('webpage').create();
+
+var names = ['yury', 'peter', 'mark', 'john', 'misha', 'jessica', 'mary', 'linda', 'lisa'];
+
+page.viewportSize = { width: 1920, height: 1080 };
+
+page.evaluate(function() {
+    document.body.bgColor = 'white';
+});
+
+names.forEach(function(name) {
+    page.open('http://localhost:3131/hello/' + name, function() {
+        //don't work..
+        page.render(name + '.png', {format: 'png', quality: '100'});
+        phantom.exit();
+    });
+});
