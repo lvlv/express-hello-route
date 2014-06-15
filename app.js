18216d879384763ff7118e452b175547b337828f
Yury Shapkarin <shapkarin@narr8.me>
2014-06-15 22:37:47 +0400

    init

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+
+app.set('port', 3131);
+
+function toUpperCaseFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+var users = [];
+
+app.get('/hello/:login', function(req,res) {
+
+  var name = req.params.login;
+
+  users.push(toUpperCaseFirstLetter(name));
+
+  res.status(200);
+  res.set('Content-type', 'text/html');
+  res.send(
+    '<title>Hi ' + toUpperCaseFirstLetter(name) + '</title>' +
+    '<html><body>' +
+        '<h1>Hello ' + toUpperCaseFirstLetter(name) + '</h1>' +
+    '</body></html>'
+  );
+
+  // uniq names
+  users = users.filter(function(elem, pos, self) {
+      return self.indexOf(elem) == pos;
+  });
+
+  console.log('login: ' + '"' + name + '"' + '\n' + 'users: ' + users + '\n');
+
+});
+
+app.get('*', function(req, res){
+  res.end('Hello World');
+});
+
+app.listen(app.get('port'), 'localhost', function(){
+  console.log('The server is running, ' +
+    ' please open your browser at http://localhost:' +
+     app.get('port'));
+});
